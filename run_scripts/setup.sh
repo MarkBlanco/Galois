17626ca87a8736d1af5bcd70abf745063a41ca79
Mark Blanco <markb1@cmu.edu>
2020-10-19 13:22:54 -0400

    add run scripts for galois testing.

---
+++ run_scripts/setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# setup for data locations outside of algo locations.
+DATADIR="/sharedstorage/markb1/GAP_data/galois_format/"
+SOURCESDIR="/sharedstorage/markb1/GAP_data/sources/"
+GKCBASE="/home/markb1/Repos/galoisExecs/"
+
+NUM_THREADS=32
+
+
+export DATADIR
+export SOURCESDIR
+export GKCBASE
+export NUM_THREADS
